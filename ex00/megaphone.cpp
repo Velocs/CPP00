327306a72e27d3a62a4558364c56bbf910e14f63
Velocs <63161372+Velocs@users.noreply.github.com>
2023-09-20 13:09:18 +0200

    Add files via upload

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main(int ac, char **av)
+{
+    int i;
+    int j;
+
+    i = 1;
+    if (ac != 1)
+    {
+    
+        while(av[i])
+        {
+            j = 0;
+            while(av[i][j])
+            {
+                std::cout << (char)toupper(av[i][j]);
+                j++;
+            }
+            i++;
+        }
+        std::cout << std::endl;
+    }
+    else
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+}
